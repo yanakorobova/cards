fd9aaac6ecd23181d75c9a7dd9f9219200091f66
yanakorobova <yanaheaven@yandex.ru>
2023-02-25 22:26:29 +0300

    init project

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './app/App';
 import reportWebVitals from './reportWebVitals';
-import {HashRouter} from "react-router-dom";
+import {BrowserRouter} from "react-router-dom";
 import {Provider} from "react-redux";
 import {store} from "./redux/store";
 
@@ -12,11 +12,11 @@ const root = ReactDOM.createRoot(
 );
 
 root.render(
-    <HashRouter>
+    <BrowserRouter>
         <Provider store={store}>
             <App />
         </Provider>
-    </HashRouter>
+    </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
