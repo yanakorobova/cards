c1c7ff7d6b0ea78bd2851865aeafbb5220f0b600
yanakorobova <yanaheaven@yandex.ru>
2023-02-27 12:06:42 +0300

    feat: added middleware and type AppThunkDispatch

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,10 +1,10 @@
-
-import {combineReducers, legacy_createStore} from "redux";
+import {AnyAction, applyMiddleware, combineReducers, legacy_createStore} from "redux";
 import {appReducer} from "./reducers/app-reducer";
 import {loginReducer} from "./reducers/login-reducer";
 import {profileReducer} from "./reducers/profile-reducer";
 import {signUpReducer} from "./reducers/signUp-reducer";
 import {TypedUseSelectorHook, useDispatch, useSelector} from "react-redux";
+import thunk, {ThunkDispatch} from "redux-thunk";
 
 const rootReducer = combineReducers({
     app: appReducer,
@@ -13,11 +13,13 @@ const rootReducer = combineReducers({
     profile: profileReducer
 })
 
-export const store = legacy_createStore(rootReducer)
+export const store = legacy_createStore(rootReducer,applyMiddleware(thunk))
+
 export type AppRootStateType = ReturnType<typeof rootReducer>
 
-export const useAppDispatch = () => useDispatch()
+export const useAppDispatch = () => useDispatch<AppThunkDispatch>()
 export const useAppSelector: TypedUseSelectorHook<AppRootStateType> = useSelector
+export type AppThunkDispatch = ThunkDispatch<AppRootStateType, any, AnyAction>
 
 //@ts-ignore
 window.store = store
